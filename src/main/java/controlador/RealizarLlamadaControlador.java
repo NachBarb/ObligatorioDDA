8e49b7d727b05f51de7e27a490d49fa3c6427488
Bardolas <martinbove1995@gmail.com>
2022-11-11 20:38:33 -0300

    Realizar llamadas Controlador WIP

---
+++ src/main/java/controlador/RealizarLlamadaControlador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import com.mycompany.ObliDDA.iu.RealizarLlamada;
+
+/**
+ *
+ * @author Martin
+ */
+public class RealizarLlamadaControlador {
+        private RealizarLlamada vista;
+
+    public RealizarLlamadaControlador(RealizarLlamada vista) {
+        this.vista = vista;
+    }
+
+}
