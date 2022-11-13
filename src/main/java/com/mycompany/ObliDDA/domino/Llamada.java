9d1fca9b68585996a3f046c66f9e3ffc98b1c821
NachBarb <ignaciobarbisancosta@gmail.com>
2022-11-13 02:49:52 -0300

    Se Siguio avanzando en la tarea en curso

--- src/main/java/com/mycompany/ObliDDA/domino/Llamada.java
+++ src/main/java/com/mycompany/ObliDDA/domino/Llamada.java
@@ -3,19 +3,30 @@ package com.mycompany.ObliDDA.domino;
 import java.util.Date;
 
 public class Llamada {
+
     private Date inicio;
     private Date fin;
     private String descripcion;
+    private Cliente cliente;
+
+    public Llamada(Date inicio, Date fin, String descripcion) {
+        this.inicio = inicio;
+        this.fin = fin;
+        this.descripcion = descripcion;
+        this.cliente = null;
+    }
 
     public Llamada() {
         this.inicio = new Date();
 
     }
-    
-    public Llamada(Date inicio, Date fin, String descripcion) {
-        this.inicio = inicio;
-        this.fin = fin;
-        this.descripcion = descripcion;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 
     public Date getInicio() {
@@ -33,10 +44,9 @@ public class Llamada {
     public void setFin(Date fin) {
         this.fin = fin;
     }
-    
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-    
-    
+
 }
